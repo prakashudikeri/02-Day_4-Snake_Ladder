587fb9a222c85552dc1afff482328eb6c0eb25b8
prakashudikeri <pu010889@gmail.com>
2022-02-03 18:20:40 +0530

    Dice rolling for Dice number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,14 +1,20 @@
 public class SnakeLadder {	
 
-	int playerPosition = 0;
+int playerPosition = 0;
 	
 	void showPosition() {
 		System.out.println("Player position is " + playerPosition);
 	}
 	
+	int rolldice() {
+		int diceNo = (int) Math.floor(Math.random() * 10) %6 + 1;
+		return diceNo;
+	}
+	
 	public static void main(String[] args) {
 		SnakeLadder player1 = new SnakeLadder();
 		player1.showPosition();
-	}
+		System.out.println("Dice number for player is " + player1.rolldice());
+	}	
 
 }
